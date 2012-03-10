3fa27ffafcbab31beee4cb99b077fbf04db1511a
Bren <bmaddox154@gmail.com>
2012-03-10 15:40:17 -0500

    Merged ModeViewController into each Mode's ViewController, Fixed bug in ProblemView

--- Classes/ChallengeViewController.m
+++ Classes/ChallengeViewController.m
@@ -13,7 +13,7 @@
 
 -(id) initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil 
 {
-    if (self = [super initWithNibName:@"ModeViewController" bundle:nibBundleOrNil]) 
+    if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) 
     {
         [instructionsLabel setText:@"Draw the arrows to the appropiate spots"];
 		isSecondTry = FALSE;
