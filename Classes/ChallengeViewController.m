a7c89b5072d75b7b9f119a27206cd0a321e131f8
Bren <bmaddox154@gmail.com>
2012-03-10 15:48:44 -0500

    Updated project files

--- Classes/ChallengeViewController.m
+++ Classes/ChallengeViewController.m
@@ -13,7 +13,7 @@
 
 -(id) initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil 
 {
-    if (self = [super initWithNibName:@"ModeViewController" bundle:nibBundleOrNil]) 
+    if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) 
     {
         
     }
