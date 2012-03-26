47ef4e816622f7f2bd34b1a8815f58a604b08e2e
Bren <bmaddox154@gmail.com>
2012-03-26 03:02:46 -0400

    Merged Identify Mode

--- Classes/IdentifyViewController.h
+++ Classes/IdentifyViewController.h
@@ -11,4 +11,13 @@
 
 @interface IdentifyViewController : ModeViewController
 
+@property (retain, nonatomic) IBOutlet UIImageView *epImage;
+@property (retain, nonatomic) IBOutlet UIImageView *npImage;
+@property (retain, nonatomic) IBOutlet UILabel *npLabel;
+@property (retain, nonatomic) IBOutlet UILabel *epLabel;
+@property (retain, nonatomic) IBOutlet UIImageView *npSuccessImage;
+@property (retain, nonatomic) IBOutlet UIImageView *epSuccessImage;
+- (IBAction)tappedNext:(UIButton *)sender;
+@property (retain, nonatomic) IBOutlet UIButton *nextButton;
+
 @end
