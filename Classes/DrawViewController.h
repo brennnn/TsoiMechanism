6a82e8d53c589796a9fbddef8859682695906773
Ian Graham <ianrgraham@gmail.com>
2012-04-02 21:10:49 -0400

    updated scoring system

--- Classes/DrawViewController.h
+++ Classes/DrawViewController.h
@@ -12,10 +12,18 @@
 @interface DrawViewController : ModeViewController <UIAlertViewDelegate>
 {
 	UIButton *hintButton;
+	int correctValue;
+	int incorrectValue;
 }
 
 @property (nonatomic, retain) IBOutlet UIButton *hintButton;
+@property (nonatomic, assign) int correctValue;
+@property (nonatomic, assign) int incorrectValue;
 
 -(IBAction)hintPressed:(id)sender;
+-(void) setDrawInstructions;
+-(void) hintPopUp;
+-(void) getCorrectScore;
+-(void) getinCorrectScore;
 
 @end
--- Classes/DrawViewController.m
+++ Classes/DrawViewController.m
@@ -9,16 +9,52 @@
 #import "DrawViewController.h"
 
 @implementation DrawViewController
-@synthesize hintButton;
+@synthesize hintButton, correctValue, incorrectValue;
 
 -(id) initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil 
 {
     if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) 
     {
 		[self setDrawInstructions];
+		//console check of what the arrow count currently is, at default before anything is drawn, is 1
+		[problemView getProblemArrowCount];
+		NSLog([NSString stringWithFormat:@"getArrowCount is: %i", [problemView getProblemArrowCount]]);
     }
     return self;
 }
+
+-(void) getinCorrectScore {
+	//determines the value of the incorrect bond 
+	if (score != 0) {
+		if ([problemView getProblemArrowCount] == 3) {
+			incorrectValue = 10;
+		}
+		if ([problemView getProblemArrowCount] == 2) {
+			incorrectValue = 25;
+		}
+		else {
+			incorrectValue = 25;
+		}
+	}
+	else {
+		incorrectValue = 0;
+	}
+	
+}
+
+-(void) getCorrectScore {
+	//determines the value of a correct value
+	if ([problemView getProblemArrowCount] == 3) {
+		correctValue = 25;
+	} 
+	if ([problemView getProblemArrowCount] == 2) {
+		correctValue = 50;
+	} 
+	else {
+		correctValue = 100;
+	}
+}
+
 
 -(void) touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     UITouch *touch = [[event allTouches] anyObject];
@@ -46,7 +82,15 @@
 }
 
 -(void) touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
-    UITouch *touch = [[event allTouches] anyObject];
+	//Determines correctValues and incorrectValues
+	[self getCorrectScore];
+	[self getinCorrectScore];
+	
+	//console check on the values of the correctValue and incorrectValue
+	NSLog([NSString stringWithFormat:@"correctValue is: %i", correctValue]);
+	NSLog([NSString stringWithFormat:@"incorrectValue is %i", incorrectValue]);	
+	
+	UITouch *touch = [[event allTouches] anyObject];
     CGPoint touchPoint = [touch locationInView:problemView];
 	//NSString *arrowCount = (NSString *) [problemView getProblemArrowCount]; // How many Arrows in the problem are needed
 	//NSString *arrowStack = (NSString *) [problemView getArrowStackCount]; // How many arrows where used
@@ -64,6 +108,8 @@
 				if([problemView doesLastArrowMatchProblem]) {
 					NSLog(@"Arrow Match");
 					[problemView showElectrophileMarker:hitbox];
+					//correct arrow drawn adds correctValue to the score
+					[self addScore:correctValue];
 				} 
 				else 
 				{
@@ -71,6 +117,8 @@
 					[problemView removeLastArrow];
 					[problemView clearNucleophileMarker];
 					[self hintPopUp];
+					//incorrect arrow drawn subtracts incorrectValue from the score
+					[self removeScore:(incorrectValue)];
 				}
             }
 			
@@ -86,12 +134,16 @@
 						[hintView show];
 						[hintView release];
 					}
+					//correct arrow drawn adds correctValue to the score
+					[self addScore:(correctValue)];
 				}
 				else
 				{
 					NSLog(@"Arrow does not Match past first");
 					[problemView removeLastArrow];
 					[self hintPopUp];
+					//incorrect arrow drawn subtracts incorrectValue from the score
+					[self removeScore:(incorrectValue)];
 				}
 				
 			}
@@ -107,6 +159,51 @@
 		}
 		
     }
+	NSLog([NSString stringWithFormat:@"arrow stack count: %i", [problemView getProblemArrowCount]]);
+}
+
+-(void)setDrawInstructions
+{
+	self.instructionsLabel.text = @"Draw arrows from the highlighted elements and bonds in order to complete the problem. Drawing an incorrect arrow will cause a hint to pop up. Click the hint button to receive a hint otherwise. When you finish drawing all the correct arrows, a confirmation to go to the next problem will appear.";
+}
+
+-(IBAction)hintPressed:(id)sender{
+	
+	[self hintPopUp];
+}
+
+- (void)alertView:(UIAlertView *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
+	// the user clicked one of the OK/Cancel buttons
+	if ([problemView doesAllArrowsMatchProblem])
+	{
+		if ([problemView getProblemArrowCount]==3) {
+			[self addScore:(25)];
+		} else {
+			[self goToNextProblem];
+		}
+	}
+	else
+	{
+		NSLog(@"cancel");
+	}
+}
+
+-(void)hintPopUp{
+	NSMutableArray *hintArray = [[NSMutableArray alloc] init];
+	
+	[hintArray addObject:@"Are you sure that is the right bond?"];
+	[hintArray addObject:@"Try a different element!"];
+	[hintArray addObject:@"How many bonds are there in this problem?"];
+	[hintArray addObject:@"Try a different bond"];
+	[hintArray addObject:@"That arrow might not go there"];
+	
+	int numObjects = [hintArray count];
+	int randomInt = arc4random() % (numObjects);
+	
+	NSString *randomHint = [NSString stringWithString:[hintArray objectAtIndex: randomInt]];
+	UIAlertView *hintView = [[UIAlertView alloc]initWithTitle:@"Hint!" message:randomHint delegate:self cancelButtonTitle:@"Return to Game" otherButtonTitles:nil];
+	[hintView show];
+	[hintView release];
 }
 
 - (void)viewDidLoad
@@ -128,46 +225,6 @@
 -(void) dealloc {
     [super dealloc];
 	[hintButton	release];
-}
-
--(void)setDrawInstructions
-{
-	self.instructionsLabel.text = @"Draw arrows from the highlighted elements and bonds in order to complete the problem. Drawing an incorrect arrow will cause a hint to pop up. Click the hint button to receive a hint otherwise. When you finish drawing all the correct arrows, a confirmation to go to the next problem will appear.";
-}
-
--(IBAction)hintPressed:(id)sender{
-	
-	[self hintPopUp];
-}
-
-- (void)alertView:(UIAlertView *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
-	// the user clicked one of the OK/Cancel buttons
-	if ([problemView doesAllArrowsMatchProblem])
-	{
-		[self goToNextProblem];
-	}
-	else
-	{
-		NSLog(@"cancel");
-	}
-}
-
--(void)hintPopUp{
-	NSMutableArray *hintArray = [[NSMutableArray alloc] init];
-	
-	[hintArray addObject:@"Are you sure that is the right bond?"];
-	[hintArray addObject:@"Try a different element!"];
-	[hintArray addObject:@"How many bonds are there in this problem?"];
-	[hintArray addObject:@"Try a different bond"];
-	[hintArray addObject:@"That arrow might not go there"];
-	
-	int numObjects = [hintArray count];
-	int randomInt = arc4random() % (numObjects);
-	
-	NSString *randomHint = [NSString stringWithFormat:[hintArray objectAtIndex: randomInt]];
-	UIAlertView *hintView = [[UIAlertView alloc]initWithTitle:@"Hint!" message:randomHint delegate:self cancelButtonTitle:@"Return to Game" otherButtonTitles:nil];
-	[hintView show];
-	[hintView release];
 }
 
 @end
